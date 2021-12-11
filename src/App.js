cec912788df68a307909b0f4cd03660aa7602966
Honshu11 <egonza67@yahoo.com>
2021-12-11 12:09:39 -0800

    added search function, finalized header font and centered pagination with color changes.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import TableRow from "./Components/TableRow";
 import axios from "axios";
 import React, {useEffect, useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import header from "./Images/starwarsheaderApi.png";
 import './App.css';
 
 function App() {
   const [characterList, setCharacterList] = useState([]);
-
   const [pageCount, setPageCount] = useState(0);
+  const [submitBoxValue, setSubmitBoxValue] = useState("");
 
   useEffect(() => {
     const getCharacters = () => {
@@ -28,15 +29,28 @@ function App() {
     }); 
   }
 
+  const pushedSubmit = (e) => {
+    e.preventDefault();
+    axios.get(`https://swapi.dev/api/people?search=${submitBoxValue}`).then((response) => {
+      setCharacterList(response.data.results);
+    })
+
+
+  }
+
+  const onInputChange = (e) => {
+    setSubmitBoxValue(e.target.value);
+  }
+
   return (
     
       <div className="container">
-        <header className="header mt-3">
-          <h1 id="sw-header-title">Star Wars API</h1>
+        <header className="header mt-3">      
+          <img src={header} alt="Star Wars API" />
         </header>
-        <div className="form-container">
-          <form className="form">
-            <input className="form-input" placeholder="Use the force"/>
+        <div className="form-container mt-3">
+          <form className="form" onSubmit={pushedSubmit}> {/*handles onSubmit changes, need function to do this */}
+            <input className="form-input" placeholder="Use the force" value={submitBoxValue} onChange={onInputChange}/>
             <button className="btn btn-warning ms-3">Search</button>
           </form>
         </div>
@@ -57,11 +71,11 @@ function App() {
             })}
           </tbody>
         </Table>
-        <nav>
+        <nav className="center">
           <ul className="pagination">
             {[...Array(pageCount)].map((e, index) => {
               return(
-                <li className="page-item"><a className="page-link" href="#" onClick={(e) => onPageClick(e, index+1)}>{index+1}</a></li>
+                <li className="page-item"><a className="page-link space-gray custom-page-link" href="#" onClick={(e) => onPageClick(e, index+1)}>{index+1}</a></li>
               )
             })}
           </ul>
